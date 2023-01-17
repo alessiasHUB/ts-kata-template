70365bc5394825add4883c3c8bffeffcc708c80b
Alessia B <borys.alessia@gmail.com>
2023-01-17 19:02:14 +0100

    poker in progress

--- src/poker-hands.ts
+++ src/poker-hands.ts
@@ -8,30 +8,20 @@
  * indicate which, if either, has a higher rank.
  * clubs, diamonds, hearts, spades = C, D, H, S
  * 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace =
- * 2, 3, 4, 5, 6, 7, 8, 9,  T,   J,    Q,    K,    A
+ * 2, 3, 4, 5, 6, 7, 8, 9, 10,  11,   12,    13,   14
  * ============================================================
  * Checks which hand is ranked higher
  * @param cards - an object containing the cards of the two players
  * @returns who the winner is and what their hand was
  */
-type CardKind = "C" | "D" | "H" | "S";
-type CardValue =
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9"
-  | "T"
-  | "J"
-  | "Q"
-  | "K"
-  | "A";
+type CardKind = "C" | "D" | "S" | "H";
+// all suites are of the same strength in poker, can remove
+const kindValue = { C: 0.1, D: 0.2, S: 0.3, H: 0.4 };
+type CardValue = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
+
 interface Card {
   cardKind: CardKind;
-  cardValue: string;
+  cardValue: CardValue;
 }
 type InputCards = {
   Black: Card[];
@@ -54,52 +44,7 @@ interface ReturnType {
   winningType: WinningType;
   winningCard: string;
 }
-const kindValue = { C: 1, D: 2, H: 3, S: 4 };
-const cardSeriesValue = {
-  2: 1,
-  3: 2,
-  4: 3,
-  5: 4,
-  6: 5,
-  7: 6,
-  8: 7,
-  9: 8,
-  T: 9,
-  J: 10,
-  Q: 11,
-  K: 12,
-  A: 13,
-};
-interface RetCardSerVal {
-  1: CardValue;
-  2: CardValue;
-  3: CardValue;
-  4: CardValue;
-  5: CardValue;
-  6: CardValue;
-  7: CardValue;
-  8: CardValue;
-  9: CardValue;
-  10: CardValue;
-  11: CardValue;
-  12: CardValue;
-  13: CardValue;
-}
-const returnCardSeriesValue: RetCardSerVal = {
-  1: "2",
-  2: "3",
-  3: "4",
-  4: "5",
-  5: "6",
-  6: "7",
-  7: "8",
-  8: "9",
-  9: "T",
-  10: "J",
-  11: "Q",
-  12: "K",
-  13: "A",
-};
+
 const scoreObject = {
   StraightFlush: ["iX", "(i+1)X", "(i+2)X", "(i+3)X", "(i+4)X"],
   FourOfAKind: ["iC", "iD", "iH", "iS"],
@@ -111,21 +56,33 @@ const scoreObject = {
   Pair: ["iX", "yX"],
   HighCard: ["iX"],
 };
-function isHighCard(inputCards: Card[]): Card {
-  let handOfCards = [];
-  let handOfKind: CardKind[] = [];
-  for (let card of inputCards) {
-    handOfCards.push(cardSeriesValue[card.cardValue]);
-    handOfKind.push(card.cardKind);
+
+//------------------------------------------------checks top card in hand
+function isHighCard(hand: Card[]): Card {
+  let handOfCards: number[] = [];
+  for (let card of hand) {
+    handOfCards.push(card.cardValue + kindValue[card.cardKind]);
   }
   let largestCard = Math.max(...handOfCards);
   let indexOfLargestCard = handOfCards.indexOf(largestCard);
-  let topCardValue: string = largestCard.toString();
-  let topCard: Card = {
-    cardValue: returnCardSeriesValue[topCardValue],
-    cardKind: handOfKind[indexOfLargestCard],
-  };
+  let topCard: Card = hand[indexOfLargestCard];
   return topCard;
+}
+//------------------------------------------------checks how many pairs in hand
+function numOfPairs(hand: Card[]): 0 | 1 | 2 {
+  const pairObj: any = {};
+  for (let card of hand) {
+    if (pairObj[card.cardValue] === undefined) {
+      pairObj[card.cardValue] = 1;
+    } else {
+      pairObj[card.cardValue]++;
+    }
+  }
+  if (pairObj.keys.length === 0) {
+    return 0;
+  } else {
+    return pairObj.keys.length;
+  }
 }
 
 function pokerHands(inputCards: InputCards): ReturnType {
